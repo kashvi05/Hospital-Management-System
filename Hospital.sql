cb5d5d0f97d3cbe237b904359efb20c558240b8b
khushi2609 <80512674+khushi2609@users.noreply.github.com>
2021-04-16 11:37:23 +0530

    Update Hospital.sql

--- Hospital.sql
+++ Hospital.sql
@@ -1,138 +1,137 @@
 create database hospital;
 use hospital;
-
-CREATE TABLE `Patient Table` (
-	`Patient Id` INT NOT NULL AUTO_INCREMENT,
-	`Patient Name` varchar(255) NOT NULL,
+#drop database hospital;
+CREATE TABLE `Patient_Table` (
+	`Patient_Id` INT NOT NULL AUTO_INCREMENT,
+	`Patient_Name` varchar(255) NOT NULL,
 	`Gender` varchar(1) NOT NULL,
 	`Age` INT NOT NULL,
-	`Blood Group` varchar(3) NOT NULL,
+	`Blood_Group` varchar(3) NOT NULL,
 	`Address` varchar(255) NOT NULL,
-	`Patient Contact No` INT NOT NULL,
-	`Residence No` INT NOT NULL,
-	`Email ID` varchar(255) NOT NULL,
-	`Emergency Number` INT NOT NULL,
-	`Emergency Contact Name` varchar(300) NOT NULL,
+	`Patient_Contact_No` INT NOT NULL,
+	`Residence_No` INT NOT NULL,
+	`Email_ID` varchar(255) NOT NULL,
+	`Emergency_Number` INT NOT NULL,
+	`Emergency_Contact_Name` varchar(300) NOT NULL,
 	`Purpose` varchar(300) NOT NULL,
-	`Emergency Patient` BOOLEAN NOT NULL,
-	PRIMARY KEY (`Patient Id`,`Patient Name`,`Patient Contact No`)
+	`Emergency_Patient` BOOLEAN NOT NULL,
+	PRIMARY KEY (`Patient_Id`,`Patient_Name`,`Patient_Contact_No`)
 );
 
-CREATE TABLE `Doctor Table` (
-	`Doctor Id` INT NOT NULL AUTO_INCREMENT,
-	`Doctor Name` varchar(255) NOT NULL,
+CREATE TABLE `Doctor_Table` (
+	`Doctor_Id` INT NOT NULL AUTO_INCREMENT,
+	`Doctor_Name` varchar(255) NOT NULL,
 	`Age` INT NOT NULL,
 	`Speciality` varchar(255) NOT NULL,
-	`Email id` varchar(255) NOT NULL,
-	`Contact No` INT NOT NULL,
-	`Office Room No` INT NOT NULL,
+	`Email_id` varchar(255) NOT NULL,
+	`Contact_No` INT NOT NULL,
+	`Office_Room_No` INT NOT NULL,
 	`Salary` FLOAT NOT NULL,
-	`Patient Id` INT NOT NULL,
-	PRIMARY KEY (`Doctor Id`,`Doctor Name`)
+	`Patient_Id` INT NOT NULL,
+	PRIMARY KEY (`Doctor_Id`,`Doctor_Name`)
 );
 
-CREATE TABLE `Nurse Table` (
-	`Nurse Id` INT NOT NULL,
-	`Nurse Name` varchar(300) NOT NULL,
+CREATE TABLE `Nurse_Table` (
+	`Nurse_Id` INT NOT NULL,
+	`Nurse_Name` varchar(300) NOT NULL,
 	`Gender` varchar(1) NOT NULL,
 	`Age` INT NOT NULL,
-	`Phone Number` INT NOT NULL,
-	`Joining Date` DATETIME NOT NULL,
-	`Shift Days` varchar(300) NOT NULL,
-	`Shift timing` TIMESTAMP NOT NULL,
+	`Phone_Number` INT NOT NULL,
+	`Joining_Date` date NOT NULL,
+	`Shift_Days` varchar(300) NOT NULL,
+	`Shift_timing` TIMESTAMP NOT NULL,
 	`Position` varchar(300) NOT NULL,
 	`Salary` FLOAT NOT NULL,
-	`Patient Id` INT NOT NULL,
-	PRIMARY KEY (`Nurse Id`)
+	`Patient_Id` INT NOT NULL,
+	PRIMARY KEY (`Nurse_Id`)
 );
 
-CREATE TABLE `Room Table` (
-	`Room No` INT NOT NULL AUTO_INCREMENT,
-	`Room type` varchar(100) NOT NULL,
-	`Number of bed` INT NOT NULL,
-	`Availibility Status` BOOLEAN NOT NULL,
-	PRIMARY KEY (`Room No`)
+CREATE TABLE `Room_Table` (
+	`Room_No` INT NOT NULL AUTO_INCREMENT,
+	`Room_type` varchar(100) NOT NULL,
+	`Number_of_bed` INT NOT NULL,
+	`Availibility_Status` BOOLEAN NOT NULL,
+	PRIMARY KEY (`Room_No`)
 );
 
-CREATE TABLE `Appointment Table` (
-	`Patient Id` INT NOT NULL UNIQUE,
-	`Patient Name` varchar(255) NOT NULL,
-	`Appointment Date` DATETIME NOT NULL,
+CREATE TABLE `Appointment_Table` (
+	`Patient_Id` INT NOT NULL UNIQUE,
+	`Patient_Name` varchar(255) NOT NULL,
+	`Appointment_Date` date NOT NULL,
 	`Time` TIMESTAMP NOT NULL,
 	`Purpose` varchar(300) NOT NULL,
-	`Patient Contact No` INT NOT NULL,
-	`Doctor Id` INT NOT NULL UNIQUE,
-	`Doctor Name` varchar(255) NOT NULL,
-	`Room No` INT NOT NULL
+	`Patient_Contact_No` INT NOT NULL,
+	`Doctor_Id` INT NOT NULL UNIQUE,
+	`Doctor_Name` varchar(255) NOT NULL,
+	`Room_No` INT NOT NULL
 );
 
 CREATE TABLE `Billing` (
-	`Bill No` INT NOT NULL AUTO_INCREMENT,
-	`Bill Date` DATETIME NOT NULL,
-	`Patient Id` INT NOT NULL,
-	`Patient Name` varchar(255) NOT NULL,
-	`Doctor Id` INT NOT NULL,
-	`Doctor's fees` FLOAT NOT NULL,
-	`Room Charges` FLOAT,
-	`Tests Charges` FLOAT,
+	`Bill_No` INT NOT NULL AUTO_INCREMENT,
+	`Bill_Date` date NOT NULL,
+	`Patient_Id` INT NOT NULL,
+	`Patient_Name` varchar(255) NOT NULL,
+	`Doctor_Id` INT NOT NULL,
+	`Doctor's_fees` FLOAT NOT NULL,
+	`Room_Charges` FLOAT,
+	`Tests_Charges` FLOAT,
 	`Tax` FLOAT NOT NULL,
-	`Total Amount` FLOAT NOT NULL,
-	PRIMARY KEY (`Bill No`)
+	`Total_Amount` FLOAT NOT NULL,
+	PRIMARY KEY (`Bill_No`)
 );
 
-CREATE TABLE `Blood Bank Records` (
-	`Date issued` DATETIME NOT NULL,
-	`Blood Group` varchar(3) NOT NULL,
-	`Pints available` INT NOT NULL,
-	PRIMARY KEY (`Blood Group`)
+CREATE TABLE `Blood_Bank_Records` (
+	`Date_issued` date NOT NULL,
+	`Blood_Group` varchar(3) NOT NULL,
+	`Pints_available` INT NOT NULL,
+	PRIMARY KEY (`Blood_Group`)
 );
 
-CREATE TABLE `Patient Diagnosis` (
-	`Diagnosis ID` INT NOT NULL AUTO_INCREMENT,
-	`Date` DATETIME NOT NULL,
-	`Diagnosis details` varchar(300) NOT NULL,
+CREATE TABLE `Patient_Diagnosis` (
+	`Diagnosis_ID` INT NOT NULL AUTO_INCREMENT,
+	`Date` date NOT NULL,
+	`Diagnosis_details` varchar(300) NOT NULL,
 	`Remarks` varchar(100) NOT NULL,
 	`Prescription` varchar(200) NOT NULL,
-	`Insurance Policy No` varchar(50) NOT NULL,
-	`Patient Id` INT NOT NULL,
-	PRIMARY KEY (`Diagnosis ID`)
+	`Insurance_Policy_No` varchar(50) NOT NULL,
+	`Patient_Id` INT NOT NULL,
+	PRIMARY KEY (`Diagnosis_ID`)
 );
 
-CREATE TABLE `Visitor's Table` (
-	`Visitor's Id` INT NOT NULL,
-	`Visitor's Name` VARCHAR(255) NOT NULL,
-	`Patient Id` INT NOT NULL,
-	`Patient Name` varchar(255) NOT NULL,
+CREATE TABLE `Visitor's_Table` (
+	`Visitor's_Id` INT NOT NULL,
+	`Visitor's_Name` VARCHAR(255) NOT NULL,
+	`Patient_Id` INT NOT NULL,
+	`Patient_Name` varchar(255) NOT NULL,
 	`Time` TIMESTAMP NOT NULL,
-	`Room No` INT NOT NULL,
-	PRIMARY KEY (`Visitor's Id`)
+	`Room_No` INT NOT NULL,
+	PRIMARY KEY (`Visitor's_Id`)
 );
 
-CREATE TABLE `Emergency Room Log` (
-	`Patient Name` varchar(255) NOT NULL,
-	`Room No` INT NOT NULL,
-	`Contact Number` INT NOT NULL
+CREATE TABLE `Emergency_Room_Log` (
+	`Patient_Name` varchar(255) NOT NULL,
+	`Room_No` INT NOT NULL,
+	`Contact_Number` INT NOT NULL
 );
 
-ALTER TABLE `Doctor Table` ADD CONSTRAINT `Doctor Table_fk0` FOREIGN KEY (`Patient Id`) REFERENCES `Patient Table`(`Patient Id`);
+ALTER TABLE `Doctor_Table` ADD CONSTRAINT `Doctor_Table_fk0` FOREIGN KEY (`Patient_Id`) REFERENCES `Patient_Table`(`Patient_Id`);
 
-ALTER TABLE `Nurse Table` ADD CONSTRAINT `Nurse Table_fk0` FOREIGN KEY (`Patient Id`) REFERENCES `Patient Table`(`Patient Id`);
+ALTER TABLE `Nurse_Table` ADD CONSTRAINT `Nurse_Table_fk0` FOREIGN KEY (`Patient_Id`) REFERENCES `Patient_Table`(`Patient_Id`);
 
-ALTER TABLE `Appointment Table` ADD CONSTRAINT `Appointment Table_fk2` FOREIGN KEY (`Patient Id`,`Patient Name`,`Patient Contact No`) REFERENCES `Patient Table`(`Patient Id`,`Patient Name`,`Patient Contact No`);
+ALTER TABLE `Appointment_Table` ADD CONSTRAINT `Appointment_Table_fk2` FOREIGN KEY (`Patient_Id`,`Patient_Name`,`Patient_Contact_No`) REFERENCES `Patient_Table`(`Patient_Id`,`Patient_Name`,`Patient_Contact_No`);
 
-ALTER TABLE `Appointment Table` ADD CONSTRAINT `Appointment Table_fk4` FOREIGN KEY (`Doctor Id`,`Doctor Name`) REFERENCES `Doctor Table`(`Doctor Id`,`Doctor Name`);
+ALTER TABLE `Appointment_Table` ADD CONSTRAINT `Appointment_Table_fk4` FOREIGN KEY (`Doctor_Id`,`Doctor_Name`) REFERENCES `Doctor_Table`(`Doctor_Id`,`Doctor_Name`);
 
-ALTER TABLE `Appointment Table` ADD CONSTRAINT `Appointment Table_fk5` FOREIGN KEY (`Room No`) REFERENCES `Room Table`(`Room No`);
+ALTER TABLE `Appointment_Table` ADD CONSTRAINT `Appointment_Table_fk5` FOREIGN KEY (`Room_No`) REFERENCES `Room_Table`(`Room_No`);
 
-ALTER TABLE `Billing` ADD CONSTRAINT `Billing_fk1` FOREIGN KEY (`Patient Id`,`Patient Name`) REFERENCES `Patient Table`(`Patient Id`,`Patient Name`);
+ALTER TABLE `Billing` ADD CONSTRAINT `Billing_fk1` FOREIGN KEY (`Patient_Id`,`Patient_Name`) REFERENCES `Patient_Table`(`Patient_Id`,`Patient_Name`);
 
-ALTER TABLE `Billing` ADD CONSTRAINT `Billing_fk2` FOREIGN KEY (`Doctor Id`) REFERENCES `Doctor Table`(`Doctor Id`);
+ALTER TABLE `Billing` ADD CONSTRAINT `Billing_fk2` FOREIGN KEY (`Doctor_Id`) REFERENCES `Doctor_Table`(`Doctor_Id`);
 
-ALTER TABLE `Patient Diagnosis` ADD CONSTRAINT `Patient Diagnosis_fk0` FOREIGN KEY (`Patient Id`) REFERENCES `Patient Table`(`Patient Id`);
+ALTER TABLE `Patient_Diagnosis` ADD CONSTRAINT `Patient_Diagnosis_fk0` FOREIGN KEY (`Patient_Id`) REFERENCES `Patient_Table`(`Patient_Id`);
 
-ALTER TABLE `Visitor's Table` ADD CONSTRAINT `Visitor's Table_fk0` FOREIGN KEY (`Patient Id`,`Patient Name`) REFERENCES `Patient Table`(`Patient Id`,`Patient Name`);
+ALTER TABLE `Visitor's_Table` ADD CONSTRAINT `Visitor's_Table_fk0` FOREIGN KEY (`Patient_Id`,`Patient_Name`) REFERENCES `Patient_Table`(`Patient_Id`,`Patient_Name`);
 
-ALTER TABLE `Visitor's Table` ADD CONSTRAINT `Visitor's Table_fk2` FOREIGN KEY (`Room No`) REFERENCES `Room Table`(`Room No`);
+ALTER TABLE `Visitor's_Table` ADD CONSTRAINT `Visitor's_Table_fk2` FOREIGN KEY (`Room_No`) REFERENCES `Room_Table`(`Room_No`);
 
-ALTER TABLE `Emergency Room Log` ADD CONSTRAINT `Emergency Room Log_fk0` FOREIGN KEY (`Room No`) REFERENCES `Room Table`(`Room No`);
-
+ALTER TABLE `Emergency_Room_Log` ADD CONSTRAINT `Emergency_Room_Log_fk0` FOREIGN KEY (`Room_No`) REFERENCES `Room_Table`(`Room_No`);
